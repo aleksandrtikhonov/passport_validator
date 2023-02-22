84e9ddd5697e40348869e7f3428cf0939199855e
atikhonov <aleksandrtikhonov91@gmail.com>
2023-02-23 01:27:37 +0300

    poetry runcommand

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from uvicorn import run
 
 app = FastAPI()
 
@@ -6,3 +7,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+def run_server():
+    run(app, host="127.0.0.1", port=8000)
+
+
+if __name__ == "__main__":
+    run_server()
